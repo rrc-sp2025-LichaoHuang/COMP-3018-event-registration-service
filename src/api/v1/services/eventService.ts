9ef2ed6287c0d928b97d4da8cdf3b241f3b95311
Zalak Punjani <zpunjani@academic.rrc.ca>
2026-01-27 15:48:56 -0600

    Feat: Added the interfaces in the eventService.ts file.

---
+++ src/api/v1/services/eventService.ts
@@ -0,0 +1,14 @@
+export interface Event {
+    id: number;
+    name: string;
+    date: string;
+    capacity: number;
+    registrationCount: number
+}
+
+export interface Attendee {
+    id: number;
+    name: string;
+    email: string
+}
+
